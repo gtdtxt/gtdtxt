be3898517242a6dbaf808a516f7de4c0e15ae5bc
Alberto <mail4alberto@gmail.com>
2016-07-07 07:55:57 -0400

    update homebrew formula

--- gtdtxt.rb
+++ gtdtxt.rb
@@ -2,15 +2,15 @@
 require 'formula'
 class Gtdtxt < Formula
   homepage 'https://github.com/gtdtxt/gtdtxt'
-  _ver = '0.10.0'
+  _ver = '0.11.0'
   version _ver
 
   if Hardware.is_64_bit?
     url "https://github.com/gtdtxt/gtdtxt/releases/download/v#{_ver}/gtdtxt-v#{_ver}-x86_64-apple-darwin.tar.gz"
-    sha256 '15e901317008dfb185f510daa3807bf09421f80ff013b1518fc417de3f85d5c5'
+    sha256 '7ccd81b1a2ff6abff69b9797e9b68a6644074575fe1f14d93d881728192794ad'
   else
     url "https://github.com/gtdtxt/gtdtxt/releases/download/v#{_ver}/gtdtxt-v#{_ver}-i686-apple-darwin.tar.gz"
-    sha256 'a29f01e6a7686d1ff0b333508ac99a9172d8bd5f4afb052f503d4f692d8a2d10'
+    sha256 '0faa14d99958cec7ba99b06c3e66c304d86e0b5521efde7b04e161ac8bc41cfb'
   end
 
   def install
